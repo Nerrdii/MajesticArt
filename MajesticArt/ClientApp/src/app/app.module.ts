f18bb424d4b12bb2b71c55b51a200a442716e54c
Joseph Davis <joseph.n.davis.98@gmail.com>
2020-06-06 18:18:16 -0500

    Add angular material

--- MajesticArt/ClientApp/src/app/app.module.ts
+++ MajesticArt/ClientApp/src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
@@ -18,6 +19,7 @@ import { AuthorizeInterceptor } from 'src/api-authorization/authorize.intercepto
   ],
   imports: [
     BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
+    BrowserAnimationsModule,
     HttpClientModule,
     FormsModule,
     ApiAuthorizationModule,
