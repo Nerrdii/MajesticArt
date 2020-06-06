ca0acf417af58d640248f2f9f4c1521087f143ff
Joseph Davis <joseph.n.davis.98@gmail.com>
2020-06-06 13:36:15 -0500

    Add category and product models

--- MajesticArt/Data/ApplicationDbContext.cs
+++ MajesticArt/Data/ApplicationDbContext.cs
@@ -12,10 +12,26 @@ namespace MajesticArt.Data
 {
     public class ApplicationDbContext : ApiAuthorizationDbContext<ApplicationUser>
     {
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+
         public ApplicationDbContext(
             DbContextOptions options,
             IOptions<OperationalStoreOptions> operationalStoreOptions) : base(options, operationalStoreOptions)
         {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<Category>().Property(c => c.Name).IsRequired();
+            builder.Entity<Category>().HasMany(c => c.Products).WithOne(p => p.Category).OnDelete(DeleteBehavior.SetNull);
+
+            builder.Entity<Product>().Property(p => p.Name).IsRequired();
+            builder.Entity<Product>().Property(p => p.Description).IsRequired();
+            builder.Entity<Product>().Property(p => p.Quantity).IsRequired();
+            builder.Entity<Product>().Property(p => p.Price).IsRequired();
+
+            base.OnModelCreating(builder);
         }
     }
 }
---
+++ MajesticArt/Models/Product.cs
@@ -0,0 +1,12 @@
+﻿namespace MajesticArt.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public Category Category { get; set; }
+        public int Quantity { get; set; }
+        public double Price { get; set; }
+    }
+}
