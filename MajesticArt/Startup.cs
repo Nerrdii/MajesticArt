9e09d8c259cbed28c3e1ab49ee054e5b942155c7
Joseph Davis <joseph.n.davis.98@gmail.com>
2020-06-06 13:34:53 -0500

    Remove email confirm for sign in

--- MajesticArt/Startup.cs
+++ MajesticArt/Startup.cs
@@ -1,9 +1,6 @@
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Identity;
-using Microsoft.AspNetCore.Identity.UI;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.SpaServices.AngularCli;
 using Microsoft.EntityFrameworkCore;
 using MajesticArt.Data;
@@ -30,7 +27,7 @@ namespace MajesticArt
                 options.UseSqlServer(
                     Configuration.GetConnectionString("DefaultConnection")));
 
-            services.AddDefaultIdentity<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = true)
+            services.AddDefaultIdentity<ApplicationUser>()
                 .AddEntityFrameworkStores<ApplicationDbContext>();
 
             services.AddIdentityServer()
