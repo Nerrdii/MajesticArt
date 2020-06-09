cf2cb4918a17a4f074687358bad37b87a013e214
Joseph Davis <joseph.davis@hrblock.com>
2020-06-09 09:55:54 -0500

    Code cleanup

--- MajesticArt/Services/CategoryService.cs
+++ MajesticArt/Services/CategoryService.cs
@@ -1,9 +1,7 @@
 ﻿using MajesticArt.Data;
 using MajesticArt.Models;
 using Microsoft.EntityFrameworkCore;
-using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 
 namespace MajesticArt.Services
