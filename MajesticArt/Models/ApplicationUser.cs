c4aa39eaeb052768a113c5941c87bcd08b9fb904
Joseph Davis <joseph.davis@hrblock.com>
2020-06-09 08:16:22 -0500

    Forgot model class

--- MajesticArt/Models/ApplicationUser.cs
+++ MajesticArt/Models/ApplicationUser.cs
@@ -1,12 +1,10 @@
 ﻿using Microsoft.AspNetCore.Identity;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace MajesticArt.Models
 {
     public class ApplicationUser : IdentityUser
     {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
     }
 }
