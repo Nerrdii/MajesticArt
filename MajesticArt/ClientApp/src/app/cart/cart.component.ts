9c59e9596208f249f839366b67b5c717428b5649
Joseph Davis <joseph.davis@hrblock.com>
2020-07-15 09:14:11 -0500

    Show cart subtotal

--- MajesticArt/ClientApp/src/app/cart/cart.component.ts
+++ MajesticArt/ClientApp/src/app/cart/cart.component.ts
@@ -3,6 +3,7 @@ import { Component, OnInit } from '@angular/core';
 import { Product } from '../models/product.model';
 import { CartService } from '../services/cart.service';
 import { Observable } from 'rxjs';
+import { tap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-cart',
@@ -11,11 +12,17 @@ import { Observable } from 'rxjs';
 })
 export class CartComponent implements OnInit {
   public items: Observable<Product[]>;
+  public subtotal = 0;
 
   constructor(private cartService: CartService) {}
 
   ngOnInit() {
-    this.items = this.cartService.items$;
+    this.items = this.cartService.items$.pipe(
+      tap((products) => {
+        this.subtotal = 0;
+        products.forEach((product) => (this.subtotal += product.price));
+      })
+    );
   }
 
   removeProduct(product: Product) {
