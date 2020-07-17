fe77730d0766c103eaeb298bdb1af6bafd089fe0
Joseph Davis <joseph.davis@hrblock.com>
2020-07-17 12:58:12 -0500

    Add order service

---
+++ MajesticArt/Services/IOrderService.cs
@@ -0,0 +1,17 @@
+﻿using MajesticArt.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MajesticArt.Services
+{
+    public interface IOrderService
+    {
+        Task<IEnumerable<Order>> GetAll();
+        Task<Order> Get(int id);
+        Task<IEnumerable<Order>> GetByUserId(string userId);
+        Task<Order> Add(Order order);
+        Task<Order> Update(Order order);
+    }
+}
---
+++ MajesticArt/Services/OrderService.cs
@@ -0,0 +1,49 @@
+﻿using MajesticArt.Data;
+using MajesticArt.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MajesticArt.Services
+{
+    public class OrderService : IOrderService
+    {
+        private readonly ApplicationDbContext context;
+
+        public OrderService(ApplicationDbContext context)
+        {
+            this.context = context;
+        }
+
+        public async Task<Order> Add(Order order)
+        {
+            await context.Orders.AddAsync(order);
+            await context.SaveChangesAsync();
+            return order;
+        }
+
+        public async Task<Order> Get(int id)
+        {
+            return await context.Orders.FindAsync(id);
+        }
+
+        public async Task<IEnumerable<Order>> GetAll()
+        {
+            return await context.Orders.ToListAsync();
+        }
+
+        public async Task<IEnumerable<Order>> GetByUserId(string userId)
+        {
+            return await context.Orders.Where(order => order.UserId == userId).ToListAsync();
+        }
+
+        public async Task<Order> Update(Order order)
+        {
+            context.Orders.Update(order);
+            await context.SaveChangesAsync();
+            return order;
+        }
+    }
+}
