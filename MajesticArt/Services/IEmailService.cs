2ce4eac0960dd1850a20cb132dc89e277329bd7e
Joseph Davis <joseph.n.davis.98@gmail.com>
2020-07-24 15:35:20 -0500

    Add SendGrid email service

---
+++ MajesticArt/Services/IEmailService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MajesticArt.Services
+{
+    public interface IEmailService
+    {
+        public Task Send(string from, string to, string subject, string textContent, string htmlContent);
+    }
+}
