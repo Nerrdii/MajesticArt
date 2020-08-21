74e263cbb79d12d599c6524502df7ef0d74d39a4
Joseph Davis <joseph.davis@hrblock.com>
2020-08-21 12:59:44 -0500

    Add migration for default product status

--- MajesticArt/Data/Migrations/20200821175854_UpdateProductChangeStatus.Designer.cs
+++ MajesticArt/Data/Migrations/20200821175854_UpdateProductChangeStatus.Designer.cs
@@ -4,14 +4,16 @@ using MajesticArt.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace MajesticArt.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20200821175854_UpdateProductChangeStatus")]
+    partial class UpdateProductChangeStatus
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
