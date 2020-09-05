25a9e87b028dcb36fdd90a924b4ac197d9bc14f8
Joseph Davis <joseph.n.davis.98@gmail.com>
2020-09-05 09:06:52 -0500

    Add taxes/shipping to order details

--- MajesticArt/ClientApp/src/app/order-details/order-details.component.ts
+++ MajesticArt/ClientApp/src/app/order-details/order-details.component.ts
@@ -2,8 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Order } from '../models/order.model';
 import { Observable } from 'rxjs';
-import { switchMap } from 'rxjs/operators';
+import { switchMap, tap } from 'rxjs/operators';
 import { OrderService } from '../admin/orders/order.service';
+import { TaxService } from '../services/tax.service';
 
 @Component({
   selector: 'app-order-details',
@@ -12,30 +13,33 @@ import { OrderService } from '../admin/orders/order.service';
 })
 export class OrderDetailsComponent implements OnInit {
   public order$: Observable<Order>;
+  public subtotal$: Observable<number>;
+  public taxes$: Observable<number>;
+  public isFreeShipping$: Observable<boolean>;
+  public shippingRate$: Observable<number>;
+  public total$: Observable<number>;
 
   constructor(
     private route: ActivatedRoute,
-    private orderService: OrderService
+    private orderService: OrderService,
+    private taxService: TaxService
   ) {}
 
   ngOnInit() {
+    this.shippingRate$ = this.taxService.getShippingRate();
+
     this.order$ = this.route.paramMap.pipe(
       switchMap((params) => {
         const id = +params.get('id');
         return this.orderService.get(id);
+      }),
+      tap((order) => {
+        const { products } = order;
+        this.subtotal$ = this.taxService.getSubtotal(products);
+        this.taxes$ = this.taxService.getTax(products);
+        this.total$ = this.taxService.getTotal(products);
+        this.isFreeShipping$ = this.taxService.isFreeShipping(products);
       })
     );
-  }
-
-  getTotal(order: Order) {
-    let total = 0;
-
-    if (order.products) {
-      order.products.forEach((product) => {
-        total += product.price;
-      });
-    }
-
-    return total;
   }
 }
